f8faf793f877b415694c3851f364003a6fe4ec57
Christian Burkhart <burkhart.christian@gmail.com>
2018-07-02 10:42:14 +0200

    feat: add single trial to logistic script

--- adaptivity_script.R
+++ adaptivity_script.R
@@ -31,9 +31,52 @@ glimpse(adaptivity_data)
 # adaptivity_data
 
 ################################
-# Funktion zur Berechnung der 
-# Akkuratheit der logitischen
-# Funktion
+# Die Akkuratheit einfach ausrechnen
+################################
+# Get size of training set specific to dataset
+smp_size <- floor(0.65 * nrow(adaptivity_data))
+
+# Get row ids of training set
+train_ind <- sample(seq_len(nrow(adaptivity_data)), size = smp_size)
+
+
+# Datasets split into training and testing
+training_set <- adaptivity_data[train_ind, ]
+testing_set <- adaptivity_data[-train_ind, ]
+
+# Bilde logistisches Regressionsmodell
+(model <- glm(able ~ correct + prior_knowledge, 
+              family = "binomial",
+              data = training_set))
+
+# Berechne Wahrscheinlichkeiten der richtigen
+# Lösung anhand des Testing Sets
+(probabilities <- predict(model, 
+                          newdata = testing_set,
+                          type = "response"))
+
+
+# Treffe hervorsagen der einzelnen Testdaten
+(predictions <- ifelse(probabilities > .8, "TRUE", "FALSE"))
+
+# Zeige die Tabelle der Hervorsgaen an
+(results <- table(predictions, testing_set$able))
+
+# Fall die Tabelle zu klein ist, gebe NA zurück
+if (nrow(results) < 2) {
+  accuracy <- results[1, 1] / sum(results)
+} else if (ncol(results) < 2) {
+  accuracy <- results[1, 1] / sum(results)
+} else {
+  accuracy <- (results[1, 1] + results[2, 2]) / sum(results)
+} 
+
+print(accuracy)
+
+
+################################
+# Boostrapping anhand von 100 
+# Durchläufen
 ################################
 get_accuracy <- function(dataset) {
   # Get size of training set specific to dataset
