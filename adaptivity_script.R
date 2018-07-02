f72e85847825b10aadc521c78d8694ba51dcc3af
Christian Burkhart <burkhart.christian@gmail.com>
2018-07-02 09:27:51 +0200

    chore: add data and script for adaptivity

---
+++ adaptivity_script.R
@@ -0,0 +1,15 @@
+#################################
+# Entwicklung einer logistischen 
+# Regression zur Berechnung der 
+# Fähigkeit, Bruchrechenaufgaben
+# der Addition zu lösen
+##################################
+
+
+# Packete laden
+library(tidyverse)
+
+# Daten laden
+adaptivity_data <- read.csv("http://ch-bu.github.io/adaptivity_data.csv", sep = ",")
+
+
